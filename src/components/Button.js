894bbe88d23529484d43543a41694fd602bf91e8
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-24 00:59:07 -0600

    finished and working

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Buttons extends Component {
+
+  render() {
+
+    return (
+
+      <div className='allButtons'>
+
+        <button className='changeButtonPrev' onClick={this.props.previous}>{`< Previous`}</button>
+
+        <div className='derp'>
+
+          <button className='noClick'>Edit</button>
+
+          <button className='noClick' onClick={this.props.delete}>Delete</button>
+
+          <button className='noClick'>New</button>
+
+        </div>
+
+        <button className='changeButtonNext' onClick={this.props.next}>Next</button>
+
+      </div>
+
+    )
+
+  }
+
+}
